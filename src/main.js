e4026c590820b054696aa8258bd7c79d1d407b56
mishu <62891208+mishuw@users.noreply.github.com>
2022-02-21 21:59:35 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
-import VueRouter from 'vue-router';
 import App from "./App.vue";
 import Bot from "./Mishuwbot.vue";
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
