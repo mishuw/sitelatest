aae18e67b6b571e9875ed6573083047450e6232f
mishu <62891208+mishuw@users.noreply.github.com>
2022-02-21 21:46:30 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Bot from "./Mishuwbot.vue";
 
 Vue.config.productionTip = false;
 
+const routers = [
+  { path: '/', component: App },
+  { path: '/r/mishu', component: Bot }
+];
+
+const router = new VueRouter({
+  routes: routers,
+  mode: 'history'
+});
+
 new Vue({
-  render: (h) => h(App)
-}).$mount("#app");
+  el: '#app',
+  router,
+  render: h => h(App)
+});
