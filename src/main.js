990d9a697b2593c5cf07e37a788f6f96fdbd5746
mishu <62891208+mishuw@users.noreply.github.com>
2022-02-21 22:06:23 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import Bot from "./Mishuwbot.vue";
 Vue.config.productionTip = false;
 
 new Vue({
-    el: '#app',
-    render: (element => element(navigation)),//this will render only navigation component. How to render pageoptions too in this call?
-    components: {App,Bot}
-})
+  el: '#app',
+  components: {
+    App,
+    Bot
+  },
+  template: '<App/>',
+  render: h => h(App)
+});
