38282a6c2855ee2fb541c29b721932d44a61cc22
mishu <62891208+mishuw@users.noreply.github.com>
2022-03-19 18:38:57 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,65 @@
 <template>
    <div class="main">
-     <h3>SALDIM İŞLERİ</h3>
+     <div class="edcph susdl">
+        <div class="rejrz"><img alt="avatar" draggable="false" class="stskr" id="sqviy" src="https://cdn.discordapp.com/embed/avatars/0.png"></div>
+        <div class="gndgn">
+          <b id="username"></b>
+            <div class="tbyhi">
+              <span id="swiom"><span class="w-3 h-3 bg-gray-500 rounded-full inline-flex ml-1 mr-1 inline-block"></span> Lanyard Yükleniyor</span><br>
+              <span id="qwanf"><span class="w-3 h-3 bg-gray-500 rounded-full inline-flex ml-1 mr-1 inline-block"></span> Lanyard Yükleniyor</span>
+            </div>
+          </div>
+    </div>
+
+    <div class="sxdfa susdl">
+      <p class="sxdfa-text mbb">Ben Kimim ?</p>
+      <p class="sxdfa-text-min text-gray-300">Bem mishu! Geliştirdiğim projeleri falan <a class="link" target="_blank" href="https://github.com/eleventhe">GitHub</a> burada paylaşıyorum bu kadar.</p>
+    </div><br>
+<div class="susdl">
+  <p class="sxdfa-text"><span style="color: #EB459E;">Kullandığım Teknolojiler</span></p>
+  <div class="grid grid-cols-2 gap-5 mt-3 sm:gird-cols-3 md:grid-cols-4">
+  <div class="udutr p-3 rounded-lg h-full hover:shadow-md flex space-x-2 text-white items-center overflow-hidden">
+    <img draggable="false" src="https://cdn.mishudev.xyz/html.png" width="21">
+    <span class="wxnmn text-gray-300">HTML5</span>
+</div>
+<div class="udutr p-3 rounded-lg h-full hover:shadow-md flex space-x-2 text-white items-center overflow-hidden">
+  <img draggable="false" src="https://cdn.mishudev.xyz/css.png" width="21">
+  <span class="wxnmn text-gray-300">CSS3</span>
+</div>
+<div class="udutr p-3 rounded-lg h-full hover:shadow-md flex space-x-2 text-white items-center overflow-hidden">
+  <img draggable="false" src="https://cdn.mishudev.xyz/tailwindLogo.png" width="21">
+  <span class="wxnmn text-gray-300">Tailwind CSS</span>
+</div>
+<div class="udutr p-3 rounded-lg h-full hover:shadow-md flex space-x-2 text-white items-center overflow-hidden">
+  <img draggable="false" src="https://cdn.mishudev.xyz/logo.png" width="21">
+  <span class="wxnmn text-gray-300">Vue.js</span>
+</div>
+<div class="udutr p-3 rounded-lg h-full hover:shadow-md flex space-x-2 text-white items-center overflow-hidden">
+  <img draggable="false" src="https://cdn.mishudev.xyz/js.png" width="21">
+  <span class="wxnmn text-gray-300">JavaScript</span>
+</div>
+<div class="udutr p-3 rounded-lg h-full hover:shadow-md flex space-x-2 text-white items-center overflow-hidden">
+  <img draggable="false" src="https://cdn.mishudev.xyz/ts.png" width="21">
+  <span class="wxnmn text-gray-300">Typescript</span>
+</div>
+<div class="udutr p-3 rounded-lg h-full hover:shadow-md flex space-x-2 text-white items-center overflow-hidden">
+  <img draggable="false" src="https://cdn.mishudev.xyz/nodejs.png" width="21">
+  <span class="wxnmn text-gray-300">Node.js</span>
+</div>
+<div class="udutr p-3 rounded-lg h-full hover:shadow-md flex space-x-2 text-white items-center overflow-hidden">
+  <img draggable="false" src="https://cdn.mishudev.xyz/electron.png" width="21">
+  <span class="wxnmn text-gray-300">Electron.js</span>
+</div>
+<div class="udutr p-3 rounded-lg h-full hover:shadow-md flex space-x-2 text-white items-center overflow-hidden">
+  <img draggable="false" src="https://cdn.mishudev.xyz/mongodb.png" width="21">
+  <span class="wxnmn text-gray-300">Mongo DB</span>
+</div>
+</div>
+</div>
+<div class="xiysw xwsda susdl">
+  <div class="footer">
+    <span class="mbb">emri abim yeni görünüşünü sevmedi. 😢</span> 
+  </div>
+</div>
 </div>
 </template>
