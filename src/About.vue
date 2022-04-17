9174075ca6973939ed37e58b800a4eebf095e5bc
mishu <62891208+mishuw@users.noreply.github.com>
2022-04-17 22:37:14 +0300

    Update About.vue

--- src/About.vue
+++ src/About.vue
@@ -5,9 +5,9 @@
             <div class="flex flex-col lg:justify-start justify-center items-center lg:items-start mt-5 lg:mt-0 w-full">
                 <div class="flex items-center">
                     <p class="flex items-center text-4xl font-bold">Hi, I'm
-                    <span class="ml-3">mishu</span></p>
-                    <span class="status-bg "><span class="ml-2 text-gray px-2 py-1 font-normal rounded-md text-sm"><i class="fa fa-circle text-gray mr-2"></i>Loading</span></span></div>
+                    <span class="ml-3">mishu</span></p></div>
                 <p class="font-light max-w-xl text-md mt-3">Hello, I'm Mishu. I'm a high school student and I've been coding for about 2-3 years.</p>
+                <span class="ml-2 status-bg text-gray px-2 py-1 font-normal rounded-md text-sm"><i class="fa fa-circle text-gray mr-2"></i>Loading</span>
             </div>
             <div class="dc-bg pb-1 rounded-b-lg w-[455px] rounded-t-lg">
                 <div>
