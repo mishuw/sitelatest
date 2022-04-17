db108d44d525b8d55ec78c8cba8aefb2b2b99900
mishu <62891208+mishuw@users.noreply.github.com>
2022-04-17 23:23:03 +0300

    Update About.vue

--- src/About.vue
+++ src/About.vue
@@ -7,8 +7,8 @@
                     <p class="flex items-center text-4xl font-bold">Hi, I'm
                     <span class="ml-3">mishu</span></p></div>
                 <p class="font-light max-w-xl text-md mt-3">Hello, I'm Mishu. I'm a high school student and I've been coding for about 2-3 years.</p>
-                <span class="ml-2 status-bg text-gray px-2 py-1 font-normal rounded-md text-sm"><i class="fa fa-circle text-gray mr-2"></i>Loading</span>
-                <span class="ml-2 spotify text-gray px-2 py-1 font-normal rounded-md text-sm"><i class="fa fa-circle text-gray mr-2"></i>Loading</span>
+                <span class="ml-2 status-bg text-gray px-2 py-1 font-normal rounded-md text-sm"><i class="fa fa-circle text-gray mr-2"></i>Loading.</span>
+                <span class="ml-2 spotify text-gray px-2 py-1 font-normal rounded-md text-sm"><i class="fa fa-circle text-gray mr-2"></i>Loading.</span>
             </div>
         </div>
     </div>
