eba2b5daee2aef37e6866071b8a522eeda14b997
mishu <62891208+mishuw@users.noreply.github.com>
2022-04-17 23:00:31 +0300

    Update About.vue

--- src/About.vue
+++ src/About.vue
@@ -8,27 +8,7 @@
                     <span class="ml-3">mishu</span></p></div>
                 <p class="font-light max-w-xl text-md mt-3">Hello, I'm Mishu. I'm a high school student and I've been coding for about 2-3 years.</p>
                 <span class="ml-2 status-bg text-gray px-2 py-1 font-normal rounded-md text-sm"><i class="fa fa-circle text-gray mr-2"></i>Loading</span>
-            </div>
-            <div class="dc-bg pb-1 rounded-b-lg w-[455px] rounded-t-lg">
-                <div>
-                    <div class="z-10" style="background-color: #b5b5e7;"></div>
-                    <div class="-mt-10 relative ml-4"><img class="border-[7px] rounded-full avatar-dc" width="85" src="https://cdn.mishudev.xyz/millie.jpg" alt=""></div>
-                    <div class="mb-3">
-                        <h1 class="ml-4 font-medium user-data text-xl mt-3">mishu<span class="text-lg text-gray-500">#6139</span></h1>
-                        <h4 class="text-sm ml-4 font-light text-gray ac-status"></h4>
-                    </div>
-                    <div class="pt-3 ml-4 mr-4 mb-3">
-                        <div class="flex">
-                        <img width="80" class="rounded shadow-xl ac-img" src="" alt="">
-                            <div class="ml-3 mt-1">
-                                <h1 class="font-medium text-sm ac-name"></h1>
-                                <h4 class="text-xs font-light text-gray ac-data"></h4>
-                                <h4 class="text-xs font-light text-gray ac-data-2"></h4>
-                                <h4 class="text-xs font-medium text-gray ac-duration"></h4>
-                        </div>
-                        </div>
-                    </div>
-                </div>
+                <span class="ml-2 spotify text-gray px-2 py-1 font-normal rounded-md text-sm"><i class="fa fa-circle text-gray mr-2"></i>Loading</span>
             </div>
         </div>
     </div>
