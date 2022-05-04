827bd4e076579351319bd0ad57e4a216cba75aca
mishu <62891208+mishuw@users.noreply.github.com>
2022-05-05 00:10:37 +0300

    Create fetchInviter.js

---
+++ public/assets/js/fetchInviter.js
@@ -0,0 +1,18 @@
+const inviteBtn = document.querySelector('.invite');
+fetch("https://discord.com/api/invite/212?with_counts=true").then(response => {
+        response.json().then(data => {
+            console.log(data)
+            document.querySelector(".icon").src = 'https://cdn.discordapp.com/icons/' + data.guild.id + '/' + data.guild.icon;
+            document.querySelector('.name').innerText = data.guild.name.length > 14 ? data.guild.name.substring(0, 14) + "..." : data.guild.name;
+            document.querySelector(".members").innerText = data.approximate_presence_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")+' Online'
+            document.querySelector(".topmembers").innerText = data.approximate_member_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")+' Members'
+            inviteBtn.addEventListener('click', () =>{
+                document.querySelector('.invite').innerText = 'Joined'
+                window.open('https://discord.com/invite'+data.code, '_BLANK')
+            })
+        })
+    })
+    inviteBtn.addEventListener('click', () =>{
+        document.querySelector('.invite').innerText = 'Joined'
+        window.open('https://discord.com/invite', '_BLANK')
+    })
