164c3c0c542d6bed91cdf0360323c2e659ceaba3
mishu <62891208+mishuw@users.noreply.github.com>
2022-05-05 00:10:07 +0300

    Update About.vue

--- src/About.vue
+++ src/About.vue
@@ -11,6 +11,17 @@
                 <span class="ml-2 spotify text-gray px-2 py-1 font-normal rounded-md text-sm"><i class="fa fa-circle text-gray mr-2"></i>Loading.</span>
                 <span class="ml-2 text-gray px-2 py-1 font-normal rounded-md text-sm discord-lg" style="cursor:pointer;" onclick="location.href='https://discord.com/users/906634054311481364'"><i class="fa-brands fa-discord"></i></span>
             </div>
+            <div class="m-2">
+    <div class="dc-bg w-[480px] h-28 flex justify-between items-center px-4 rounded-b-lg">
+        <div class="flex"><img width="78" class="rounded-xl icon" src="https://cdn.mishudev.xyz/millie.jpg" style="border-radius:25px;">
+            <div class="ml-3 mt-1">
+                <h1 class="font-bold name text-[25px]">eleven</h1>
+                <div><i class="fa fa-circle text-online text-xs mr-1 relative"></i><span class="text-xs members font-medium">0 Online</span><i class="fa fa-circle ml-3 text-offline text-xs mr-1 relative"></i><span class="text-xs topmembers font-medium">0 Members</span></div>
+            </div>
+        </div>
+        <div><button class="bg-[#2D7D46] invite text-white font-medium px-10 hover:bg-[#27693b] transition-all py-2 rounded" style="cursor:pointer;">Join</button></div>
+    </div>
+</div>
         </div>
     </div>
 </div>
