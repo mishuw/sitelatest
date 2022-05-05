41e3550645e18741648d658e62bf5a044a43a322
mishu <62891208+mishuw@users.noreply.github.com>
2022-05-05 16:14:18 +0300

    Update fetchInviter.js

--- public/assets/js/fetchInviter.js
+++ public/assets/js/fetchInviter.js
@@ -1,5 +1,6 @@
 const inviteBtn = document.querySelector('.invite');
 fetch("https://discord.com/api/invite/212?with_counts=true", {
+        method: 'GET',
         headers: {
                   'Content-Type': 'application/json'
                  },
