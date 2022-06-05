47e1583e613fb1d1c8b1c17401f69b402d989ac4
mishu <62891208+mishuw@users.noreply.github.com>
2022-06-05 20:12:05 +0300

    Update Navbar.vue

--- src/Navbar.vue
+++ src/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="navbar sticky-nav mt-2">
         <p>Mishudev.xyz</p>
-	<button class="navbar-btns mr-1 redirectbtn">Stranger Things</button>
+	<button class="navbar-btns mr-1 redirectbtn" onclick="location.href='https://stranger-things.mishudev.xyz'">Stranger Things</button>
 	<button id="switch-mode-btn" class="navbar-btns switch-mode-btn transition-shadow hover:shadow-md mr-1"><i class="fa-solid fa-moon"></i></button>
     </div>
 </template>
