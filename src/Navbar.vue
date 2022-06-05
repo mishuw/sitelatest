da9e6d453d703c0d12ffb39b9815fa2cf85721b6
mishu <62891208+mishuw@users.noreply.github.com>
2022-06-05 20:01:51 +0300

    Update Navbar.vue

--- src/Navbar.vue
+++ src/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="navbar sticky-nav mt-2">
         <p>Mishudev.xyz</p>
+	<button class="navbar-btns mr-1 cdnL"><i class="fa-solid fa-moon"></i></button>
 	<button id="switch-mode-btn" class="navbar-btns switch-mode-btn transition-shadow hover:shadow-md mr-1"><i class="fa-solid fa-moon"></i></button>
     </div>
 </template>
