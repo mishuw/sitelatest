0cfc66266340ff09c27e91f98f8de111a0665cb0
mishu <62891208+mishuw@users.noreply.github.com>
2022-06-05 19:58:51 +0300

    Update Navbar.vue

--- src/Navbar.vue
+++ src/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="navbar sticky-nav mt-2"><p>Mishudev.xyz</p>
-    		<a href="https://stranger-things.mishudev.xyz" class="navbar-btns transition-shadow hover:shadow-md mr-1 cdnL">STRANGER THINGS</a>
-		<button id="switch-mode-btn" class="navbar-btns switch-mode-btn transition-shadow hover:shadow-md mr-1"><i class="fa-solid fa-moon"></i></button>
+    <div class="navbar sticky-nav mt-2">
+        <p>Mishudev.xyz</p>
+    	<a href="https://stranger-things.mishudev.xyz" class="navbar-btns transition-shadow hover:shadow-md mr-1 cdnL">STRANGER</a>
+	<button id="switch-mode-btn" class="navbar-btns switch-mode-btn transition-shadow hover:shadow-md mr-1"><i class="fa-solid fa-moon"></i></button>
     </div>
 </template>
