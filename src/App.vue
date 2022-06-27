8eca6df4731300eaa2f1c81f820c29b9981f3de9
mishu <62891208+mishuw@users.noreply.github.com>
2022-06-27 15:34:27 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@
       <div class="dc-bg pb-1 rounded-b-lg rounded-t-lg">
     <div>
         <div class="mb-3">
-            <div class="banner" style="height:136.1px;width:340px;border-radius:2%;background:url('https://cdn.mishudev.xyz/millie.jpg') center center no-repeat;background-size:contain;background-attachment:fixed;background-position:center;">
+            <div class="banner" style="height:136px;width:340px;border-radius:2%;background:url('https://cdn.mishudev.xyz/millie.jpg') center center no-repeat;background-size:contain;background-attachment:fixed;background-position:center;">
                 <div class="mt-12 relative ml-4" style="padding-top:75px;">
                 <img class="discord_user_img bg-[#EEE8ED]" draggable="false" width="95" src="" alt="mishu avatar" style="border-radius:999%;">
                 </div>
